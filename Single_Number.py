c0cf971d5c6036529d5c2d55d96220b25e44b7ff
dkhobragade <dikshantkhobragade12@gmail.com>
2025-04-24 15:23:46 +0530

    LeetCode 11 - Single Number III

--- Single_Number.py
+++ Single_Number.py
@@ -1,8 +1,14 @@
-nums = [2, 2, 1]
+# nums = [2, 2, 1]
+#
+#
+# for i in nums:
+#     if nums.count(i)==1:
+#         print(i)
+#         break
 
-
+nums = [1,2,1,3,2,5]
+a=[]
 for i in nums:
     if nums.count(i)==1:
-        print(i)
-        break
-
+        a.append(i)
+print(a)
